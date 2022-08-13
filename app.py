f992bf97b6b5074d38b9e2cfa9bf4c14210c0707
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-13 17:25:46 +0600

    created SQLite DB

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
+from curses.ascii import NUL
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+
+class Todo(db.Model):
+    sl_no = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(200), nullable = False)
+    desc = db.Column(db.String(500), nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return print(f"{self.sl_no}- {self.title}")
 
 
 @app.route("/second-route")
