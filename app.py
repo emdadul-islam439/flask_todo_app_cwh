2a6b6fe94c44cd19024929cf4ba5110f57dfae66
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-13 14:39:54 +0600

    added a second route

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def hello_world():
     return "hello world"
 
+
+@app.route("/second-route")
+def second_route():
+    return "this is the second page"
+
+
 if __name__ == "__main__":
     app.run(debug = True, port = 8000)
